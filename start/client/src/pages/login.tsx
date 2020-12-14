e11e55b070f9b982763c37157dbee44fef3cc60a
VirtualVasquez <mxv647@med.miami.edu>
2020-12-13 22:34:38 -0500

    it's over

--- start/client/src/pages/login.tsx
+++ start/client/src/pages/login.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { gql, useMutation } from '@apollo/client';
 
 import { LoginForm, Loading } from '../components';
+import { isLoggedInVar } from '../cache';
 import * as LoginTypes from './__generated__/login';
 
 export const LOGIN_USER = gql`
@@ -23,7 +24,13 @@ export default function Login() {
       onCompleted({ login }) {
         localStorage.setItem('token', login.token as string);
         localStorage.setItem('userId', login.id as string);
+        isLoggedInVar(true);
       }
     }
   );
+
+  if (loading) return <Loading />;
+  if (error) return <p>An error occurred</p>;
+
+  return <LoginForm login={login} />;
 }
